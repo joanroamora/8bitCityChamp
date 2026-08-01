b6324ab4f0d220c5c455dba8e214f26d7f07d6b4
Joan Roa <joanroamora@gmail.com>
2026-08-01 00:53:54 -0500

    fix: cleanup GCP orphan resources and configure persistent GCS backend for Terraform state

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_version = ">= 1.3.0"
+  backend "gcs" {
+    bucket = "8bitcitychamp-tfstate-bitcitychamp-project"
+    prefix = "terraform/state"
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
